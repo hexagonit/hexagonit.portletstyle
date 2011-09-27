b436d77597bbc6c48dc772e0ac91744e5f716292
Nejc Zupan <nejc.zupan@gmail.com>
2011-09-27 21:54:15 +0200

    support for adding new portlets, for which one can select a css_class -> so far only the recent-items portlets is supported

--- hexagonit/portletclass/patcher.py
+++ hexagonit/portletclass/patcher.py
@@ -1,3 +1,17 @@
 def get_css_class(self):
     return self.data.css_class
 
+
+def base_assignment__init__(self, *args, **kwargs):
+    self.css_class = kwargs.get('css_class', 5)
+
+
+from plone.app.portlets.portlets import base
+def recent_assignment__init__(self, *args, **kwargs):
+    base.Assignment.__init__(self, *args, **kwargs)
+    self.count = kwargs.get('count', 5)
+
+
+from plone.app.portlets.portlets.recent import Assignment
+def recent_create(self, data):
+    return Assignment(count=data.get('count', 5), css_class=data.get('css_class', 5))
