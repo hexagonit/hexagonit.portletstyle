3d0c2f5161ff84dbdd6de2cbbf3a798d336e3d6e
Nejc Zupan <nejc.zupan@gmail.com>
2011-09-27 21:40:17 +0200

    patcher.py cleanup, removing stuff that should not have been commited in the first place

--- hexagonit/portletclass/patcher.py
+++ hexagonit/portletclass/patcher.py
@@ -1,8 +1,3 @@
 def get_css_class(self):
     return self.data.css_class
 
-from zope import schema
-css_class = schema.Int(title=u'CSS class id',
-                description=u'Css class description.',
-                required=True,
-                default=5)
