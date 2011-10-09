ff05d27dd8e996e6b8920b9e51403fe6e085f9d3
Nejc Zupan <nejc.zupan@gmail.com>
2011-10-09 16:12:59 +0200

    more work on est_style_selection.py

--- hexagonit/portletstyle/tests/test_style_selection.py
+++ hexagonit/portletstyle/tests/test_style_selection.py
@@ -1,11 +1,11 @@
 # -*- coding: utf-8 -*-
 """Tests for portlet style selection on a portlet."""
 
-from hexagonit.portletstyle.tests.base import IntegrationTestCase
 from Products.CMFCore.utils import getToolByName
+from hexagonit.portletstyle.tests.base import IntegrationTestCase
+from plone.app.portlets.portlets import recent
 from plone.portlets.interfaces import IPortletType
 from zope.component import getUtility
-from plone.app.portlets.portlets import recent
 
 import unittest2 as unittest
 
@@ -18,22 +18,40 @@ class TestSelectStyle(IntegrationTestCase):
         # shortcuts
         self.portal = self.layer['portal']
         self.installer = getToolByName(self.portal, 'portal_quickinstaller')
-        
+        self.mapping = self.portal.restrictedTraverse('++contextportlets++plone.leftcolumn')
+
         # remove all portlets already assigned to left column
-        mapping = self.portal.restrictedTraverse('++contextportlets++plone.leftcolumn')
-        for m in mapping.keys():
-            del mapping[m]
+        for m in self.mapping.keys():
+            del self.mapping[m]
 
-        # add recent-items portlet
-        portlet = getUtility(IPortletType, name='portlets.Recent')
-        addview = mapping.restrictedTraverse('+/' + portlet.addview)
-        addview.createAndAdd(data={})
-        self.assertEquals(len(mapping), 1)
-        self.assertTrue(isinstance(mapping.values()[0], recent.Assignment))
+    def _add_portlet(self, name='portlets.Recent', style=None):
+        style = style or ''
+        portlet = getUtility(IPortletType, name=name)
 
+        addview = self.mapping.restrictedTraverse('+/' + portlet.addview)
+        addview.createAndAdd(data={'portlet_style': style})
 
-    def test_foo(self):
-        print 'a'
+    def test_select_style(self):
+        """Test that selected style is present in portlet's HTML."""
+        # add portlet
+        self._add_portlet(style='noheader')
+        self.assertEquals(len(self.mapping), 1)
+        self.assertIsInstance(self.mapping.values()[0], recent.Assignment)
+
+        # is portlet really there?
+        self.assertEquals(len(self.mapping), 1)
+        self.assertIsInstance(self.mapping.values()[0], recent.Assignment)
+
+    def test_no_style_selected(self):
+        """Test that nothing breaks if no style was selected."""
+        # add portlet
+        self._add_portlet(style='noheader')
+        self.assertEquals(len(self.mapping), 1)
+        self.assertIsInstance(self.mapping.values()[0], recent.Assignment)
+
+        # is portlet really there?
+        self.assertEquals(len(self.mapping), 1)
+        self.assertIsInstance(self.mapping.values()[0], recent.Assignment)
 
 
 def test_suite():
