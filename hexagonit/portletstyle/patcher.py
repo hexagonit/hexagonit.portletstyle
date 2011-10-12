a8d8202a7af8097696d370e68160440b361cd980
Nejc Zupan <nejc.zupan@gmail.com>
2011-10-12 16:26:37 +0200

    patch Events portlet

--- hexagonit/portletstyle/patcher.py
+++ hexagonit/portletstyle/patcher.py
@@ -1,3 +1,8 @@
+from plone.app.portlets.portlets import base
+from plone.app.portlets.portlets import events
+from plone.app.portlets.portlets import recent
+
+
 def get_portlet_style(self):
     return self.data.portlet_style
 
@@ -6,12 +11,18 @@ def base_assignment__init__(self, *args, **kwargs):
     self.portlet_style = kwargs.get('portlet_style', '')
 
 
-from plone.app.portlets.portlets import base
+# portlet.Recent
 def recent_assignment__init__(self, *args, **kwargs):
     base.Assignment.__init__(self, *args, **kwargs)
     self.count = kwargs.get('count', 5)
-
-
-from plone.app.portlets.portlets.recent import Assignment
 def recent_create(self, data):
-    return Assignment(count=data.get('count', 5), portlet_style=data.get('portlet_style', ''))
+    return recent.Assignment(count=data.get('count', 5), portlet_style=data.get('portlet_style', ''))
+
+
+# portlet.Events
+def events_assignment__init__(self, *args, **kwargs):
+    base.Assignment.__init__(self, *args, **kwargs)
+    self.count = kwargs.get('count', 5)
+    self.state = kwargs.get('state', ('published', ))
+def events_create(self, data):
+    return events.Assignment(count=data.get('count', 5), state=data.get('state', ('published', )), portlet_style=data.get('portlet_style', ''))
