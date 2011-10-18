f7926aba21c48cc2a8cced6ff70a4f6157319513
Nejc Zupan <nejc.zupan@gmail.com>
2011-10-18 17:29:40 +0200

    initial add of css that supports default styles

--- hexagonit/portletstyle/tests/test_setup.py
+++ hexagonit/portletstyle/tests/test_setup.py
@@ -33,6 +33,13 @@ class TestSetup(IntegrationTestCase):
         from plone.browserlayer import utils
         self.failUnless(IPortletStyleLayer in utils.registered_layers())
 
+    # cssregistry.xml
+    def test_css_registered(self):
+        """Test if portletstyle.s css file is registered with portal_css."""
+        resources = self.portal.portal_css.getResources()
+        ids = [r.getId() for r in resources]
+        self.failUnless('++resource++portletstyle.css' in ids, 'portletstyle.css not found in portal_css')
+
 
 def test_suite():
     """This sets up a test suite that actually runs the tests in the class
