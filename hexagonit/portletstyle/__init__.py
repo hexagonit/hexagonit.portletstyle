0d4eaaeb1315ded4af24ef2658407ddb7faf9a29
Nejc Zupan <nejc.zupan@gmail.com>
2011-10-24 17:49:01 +0200

    patch render_cachekey so caches get invalidated when choosing a different portlet style

--- hexagonit/portletstyle/__init__.py
+++ hexagonit/portletstyle/__init__.py
@@ -1,12 +1,15 @@
 # -*- coding: utf-8 -*-
 """Init and utils."""
 
+from Acquisition import aq_inner
+from Products.CMFCore.utils import getToolByName
+from plone.app.portlets import cache
+from plone.app.portlets.cache import get_language
 from plone.app.portlets.portlets.base import Assignment
 from plone.portlets import interfaces
 from zope.i18nmessageid import MessageFactory
 from zope.interface import Interface
 from zope.schema import Choice
-
 
 PortletStyleMessageFactory = MessageFactory('hexagonit.portletstyle')
 Assignment.portlet_style = -1
@@ -24,3 +27,23 @@ class IPortletDataProvider(Interface):
         required=False,
     )
 interfaces.IPortletDataProvider = IPortletDataProvider
+
+
+def new_render_cachekey(fun, self):
+    context = aq_inner(self.context)
+
+    def add(brain):
+        path = brain.getPath().decode('ascii', 'replace')
+        return "%s\n%s\n\n" % (path, brain.modified)
+    fingerprint = "".join(map(add, self._data()))
+
+    anonymous = getToolByName(context, 'portal_membership').isAnonymousUser()
+    return "".join((
+        getToolByName(aq_inner(self.context), 'portal_url')(),
+        get_language(aq_inner(self.context), self.request),
+        str(anonymous),
+        self.manager.__name__,
+        self.data.__name__,
+        self.data.portlet_style or "",
+        fingerprint))
+cache.render_cachekey = new_render_cachekey
