73b22f85df5ca41fc38341023f993238476ef622
Nejc Zupan <nejc.zupan@gmail.com>
2011-11-25 17:42:17 +0100

    add unit tests for vocabulary

--- hexagonit/portletstyle/tests/test_vocabulary.py
+++ hexagonit/portletstyle/tests/test_vocabulary.py
@@ -5,7 +5,48 @@ from hexagonit.portletstyle.tests.base import IntegrationTestCase
 from zope.component import getUtility
 from zope.schema.interfaces import IVocabularyFactory
 
+import mock
 import unittest2 as unittest
+
+styles = 'hexagonit.portletstyle.interfaces.IPortletStyles.portlet_styles'
+
+
+class TestVocabularyUnit(unittest.TestCase):
+    """Light-weight unit tests for how values from control panel get parsed."""
+
+    def _get_terms(self):
+        """Prepare an instance of StylesVocabulary and return it's terms."""
+        from hexagonit.portletstyle.vocabulary import StylesVocabulary
+        return list(StylesVocabulary()(None))
+
+    @mock.patch('zope.component.getUtility')
+    def test_empty(self, utility):
+        """Test that we have the 'no style' vocabulary item available even
+        if there are no styles saved in control panel."""
+        utility.return_value = {styles: []}
+        terms = self._get_terms()
+
+        # only the "no style" term is here, which is injected as the first item
+        self.assertEquals(1, len(terms))
+        self.assertEquals("No style", terms[0].title)
+
+    @mock.patch('zope.component.getUtility')
+    def test_filtering_invalid_delimiters(self, utility):
+        """Test that invalid delimiters don't break the vocabulary but rather
+        simply get filtered out.
+        """
+        utility.return_value = {styles: [
+            'no_delimiter',
+            'valid delimiter|Valid delimiter',
+            'too|much|delimiters',
+        ]}
+        terms = self._get_terms()
+
+        # apart from the "no style" term, we should have one more term
+        # for one valid line in styles specified above; other lines get skipped
+        self.assertEquals(2, len(terms))
+        self.assertEquals("No style", terms[0].title)
+        self.assertEquals("Valid delimiter", terms[1].title)
 
 
 class TestVocabularyIntegration(IntegrationTestCase):
